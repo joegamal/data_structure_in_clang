9999b0f959418e2a1c0502ee493912448cc4918a
joegamal <yusufmagamalth@gmail.com>
2025-04-14 23:47:02 +0200

    stack implementation with array

---
+++ datastructure/src/main/java/org/fromScratch/Main.java
@@ -0,0 +1,61 @@
+package org.fromScratch;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Stackk<Integer> stack = new Stackk<>();
+//
+//        System.out.println(stack.stackSize);  // print 0
+//
+//        System.out.println(stack.isempty());  // print true
+//
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//        stack.add(5);
+//        stack.add(6);
+//        stack.add(7);
+//
+//        System.out.println(stack.stackSize); // print 36
+//
+//        System.out.println(stack.isempty()); // print false
+//
+//        System.out.println(stack.top()); // prints 7
+//
+//        System.out.println(stack.pop()); // prints 7
+//
+//        System.out.println(stack.top()); // prints 6
+
+        
+    }
+}
