c1c04a3ffed4b982497ed6d2805330f72b164be0
joegamal <yusufmagamalth@gmail.com>
2025-05-07 19:37:51 +0300

    ArrayList implementation

---
+++ demo/src/main/java/com/frm/Main.java
@@ -0,0 +1,23 @@
+package com.frm;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //arrayList<Integer> arrayList = new arrayList<Integer>(); 
+        //
+        //for(int i = 1; i <= 10; i++) {
+        //    arrayList.add(i);
+        //}
+        //
+        //
+        //arrayList.removeAt(5);
+        //
+        //arrayList.remove(8);
+        //
+        //arrayList.display();
+
+
+    }
+}
